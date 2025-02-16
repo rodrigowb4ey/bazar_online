b0580b0e9ee32a75c2113109821f1d63ae6ade00
Rodrigo Bezerra <rodrigobezerrasaraiva@gmail.com>
2025-02-16 19:27:13 -0300

    Adding schemas for creating and updating Category, Catalog and Product

--- bazar_online_api/app/core/schemas.py
+++ bazar_online_api/app/core/schemas.py
@@ -15,3 +15,53 @@ class UserUpdate(BaseModel):
     username: str | None = None
     email: str | None = None
     password: str | None = None
+
+
+class CategoryCreate(BaseModel):
+    """Schema for creating Category instances."""
+
+    name: str
+    owner_id: int
+
+
+class CategoryUpdate(BaseModel):
+    """Schema for updating Category instances."""
+
+    name: str | None = None
+
+
+class ProductCreate(BaseModel):
+    """Schema for creating a Product."""
+
+    name: str
+    description: str | None = None
+    price: float
+    catalog_id: int
+    category_id: int
+    owner_id: int
+
+
+class ProductUpdate(BaseModel):
+    """Schema for updating a Product."""
+
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    catalog_id: int | None = None
+    category_id: int | None = None
+    owner_id: int | None = None
+
+
+class CatalogCreate(BaseModel):
+    """Schema for creating a Catalog."""
+
+    name: str
+    description: str | None = None
+    owner_id: int
+
+
+class CatalogUpdate(BaseModel):
+    """Schema for updating a Catalog."""
+
+    name: str | None = None
+    description: str | None = None
