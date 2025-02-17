a8c100123f44f41d23af042982b5edd24ef7aee9
Rodrigo Bezerra <rodrigobezerrasaraiva@gmail.com>
2025-02-17 16:25:01 -0300

    Adding 'override_get_session' fixture

--- bazar_online_api/tests/conftest.py
+++ bazar_online_api/tests/conftest.py
@@ -1,7 +1,10 @@
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Generator
 
+import pytest
 import pytest_asyncio
 from app.core.models import Base
+from app.infra.database import get_session
+from app.main import app
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
@@ -34,3 +37,13 @@ async def session(engine: AsyncEngine) -> AsyncGenerator[AsyncSession, None]:
     async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
     async with async_session() as session:
         yield session
+
+
+@pytest.fixture(autouse=True)
+def override_get_session(session: AsyncSession) -> Generator[None, None, None]:
+    """Automatically override the get_session dependency for the FastAPI app.
+    with the test session provided by the 'session' fixture.
+    """  # noqa: D205
+    app.dependency_overrides[get_session] = lambda: session
+    yield
+    app.dependency_overrides.pop(get_session, None)
