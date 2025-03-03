4d7c4eb8b67c791f0c249f3346d2abb9d6598219
Rodrigo Bezerra <rodrigobezerrasaraiva@gmail.com>
2025-03-03 11:59:50 -0300

    Adding validation for fields in UserCreate schema + Creating Token schema

--- bazar_online_api/app/core/schemas.py
+++ bazar_online_api/app/core/schemas.py
@@ -1,12 +1,30 @@
-from pydantic import BaseModel
+from typing import Literal
+
+from pydantic import BaseModel, Field, field_validator
+
+from app.core.security import get_password_hash
 
 
 class UserCreate(BaseModel):
     """Schema for creating User instances."""
 
-    username: str
     email: str
-    password: str
+    username: str = Field(min_length=1)
+    password: str = Field(min_length=1)
+
+    @field_validator('username')
+    @classmethod
+    def username_validate(cls, v: str) -> str:
+        """Validation for 'username' field."""
+        if ' ' in v:
+            raise ValueError('username field cannot have blank spaces.')
+        return v
+
+    @field_validator('password')
+    @classmethod
+    def password_validate(cls, v: str) -> str:
+        """Validation for 'password' field."""
+        return get_password_hash(v)
 
 
 class UserUpdate(BaseModel):
@@ -65,3 +83,10 @@ class CatalogUpdate(BaseModel):
 
     name: str | None = None
     description: str | None = None
+
+
+class Token(BaseModel):
+    """Schema for access token."""
+
+    token_type: Literal['bearer'] = 'bearer'  # noqa: S105
+    access_token: str
